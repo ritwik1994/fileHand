106034861f7725a09663fd60e90e52160e8f5dab
ritwik <ritwikenator@gmail.com>
2018-05-24 17:53:45 +0530

    restCall implemented

--- RecyclerTutorial/app/src/main/java/com/example/root/recyclertutorial/Signup.java
+++ RecyclerTutorial/app/src/main/java/com/example/root/recyclertutorial/Signup.java
@@ -1,6 +1,7 @@
 package com.example.root.recyclertutorial;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,14 @@ import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Signup extends AppCompatActivity {
@@ -104,7 +113,81 @@ public class Signup extends AppCompatActivity {
 
         if (cancel) {
         } else {
-            
+            CreateNewUser createNewUser = new CreateNewUser();
+            createNewUser.execute();
+        }
+
+    }
+
+    public class CreateNewUser extends AsyncTask<String, Void, Void> {
+        BufferedReader reader;
+        HttpURLConnection urlConnection;
+        Boolean postexec = false;
+        String valueError="";
+        public String HrEMailData,PAssText;
+
+        @Override
+        protected void onPreExecute() {
+            HrEMailData =  HrEmailEdit.getText().toString();
+            PAssText = PassEdit.getText().toString();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+
+                URL url = new URL("http://127.0.0.1:8000/hr/");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+//                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(true);
+                urlConnection.connect();
+                OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+                JSONObject userData = new JSONObject();
+                userData.put("username",HrEMailData);
+                userData.put("email",HrEMailData);
+                userData.put("password",PAssText);
+                JSONObject data = new JSONObject();
+                data.put("company_name", HrEMailData);
+                data.put("email_confirmed", false);
+                data.put("user", userData);
+                wr.write(data.toString());
+                wr.flush();
+                wr.close();
+
+                StringBuilder sb = new StringBuilder();
+                int responseCode = urlConnection.getResponseCode();
+                InputStream _is;
+                if (urlConnection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                    reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                } else {
+                    reader = new BufferedReader(new InputStreamReader(urlConnection.getErrorStream()));
+                }
+                if(responseCode == 200 || responseCode == 201 || responseCode == 500) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    reader.close();
+                    valueError = sb.toString();
+
+                    return null;
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+
+
         }
 
     }
